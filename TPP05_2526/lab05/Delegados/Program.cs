a65a7cd2758d8cfa9d0ccef17361d7e6d08e2e20
uo270457 <uo270457@uniovi.es>
2026-02-20 16:47:53 +0100

    Base lab05, queda obligatorio

---
+++ TPP05_2526/lab05/Delegados/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Delegados;
+
+class Program
+{
+
+    // Definimos un tipo delegado (delegate) que representa cualquier método compatible
+    // con la firma: recibe 1 int y devuelve un int.
+    // Lo tipos delegados son nominales.
+    // ¿Qué posibilitan los tipos delegados?
+    public delegate int Function(int n1);
+
+    public delegate R GenericFunction<T, R>(T a);
+    public delegate R GenericFunction<T1, T2, R>(T1 a, T2 b);
+
+    //¿Cómo se definiría el delegado para el método Concatenar, Multiplica y Media?
+
+
+    static void Main()
+    {
+        // El método Cuadrado es compatible con el delegado Function
+        Function cuadrado = Cuadrado;
+        Console.WriteLine(cuadrado(5));
+
+        // Func admite de 0 a 16 parámetros de entrada y siempre devuelve un valor (TResult).
+        Func<int, int> cuadradoFunc = Cuadrado;
+        Console.WriteLine(cuadradoFunc(5));
+
+        Func<string, string, string> concatenarFunc = Concatenar;
+        Console.WriteLine(concatenarFunc("Hola ", "Mundo"));
+
+        Func<int, int, int> multiplicarFunc = Multiplicar;
+        Console.WriteLine(multiplicarFunc(3, 4));
+
+        Func<int, int, double> mediaFunc = Media;
+        Console.WriteLine(mediaFunc(10, 20));
+
+
+        // Predicate admite único parámetro y siempre devuelve bool.
+        Predicate<int> esParPredicate = EsPar;
+        Console.WriteLine(esParPredicate(cuadrado(5))); // false
+        Console.WriteLine(esParPredicate(cuadrado(4))); // true
+
+        // Action admite de 0 a 16 parámetros de entrada y el retorno es void.
+        Action<string> imprimirAction = Imprimir;
+        imprimirAction(concatenarFunc("Hola ", "Mundo"));
+
+    }
+    static int Cuadrado(int a)
+    {
+        return a * a;
+    }
+
+    static string Concatenar(string a, string b)
+    {
+        return a + b;
+    }
+
+    static int Multiplicar(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+
+    static double Media(int a, int b)
+    {
+        return (a + b) / 2.0;
+    }
+
+    static void Imprimir(string mensaje)
+    {
+        Console.WriteLine(mensaje);
+    }
+
+    static bool EsPar(int n)
+    {
+        return n % 2 == 0;
+    }
+
+}
---
+++ TPP05_2526/lab05/Transacciones/Program.cs
@@ -0,0 +1,250 @@
+﻿namespace Transacciones;
+
+
+
+
+
+
+/// <summary>
+/// Funciones de orden superior: Map, Filter, Reduce y Zip.
+/// class vs record y uso de with
+/// </summary>
+public class Program
+{
+
+
+    /// <summary>
+    /// Map es una función de orden superior que transforma uno a uno los elementos de una secuencia
+    /// </summary>
+    /// <typeparam name="T1">Tipo de la secuencia de entrada</typeparam>
+    /// <typeparam name="T2">Tipo de la secuencia de salida</typeparam>
+    /// <param name="secuencia">Secuencia de entrada</param>
+    /// <param name="funcion">La función que transforma un elemento de T1 en un elemento de T2</param>
+    /// <returns></returns>
+    public static IEnumerable<T2> Map<T1, T2>(IEnumerable<T1> secuencia, Func<T1, T2> funcion)
+    {
+        IList<T2> secuenciaResultante = new List<T2>();
+
+        foreach (T1 elemento in secuencia)
+        {
+            T2 transformado = funcion(elemento);
+            secuenciaResultante.Add(transformado);
+        }
+        return secuenciaResultante;
+    }
+
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> secuencia, Predicate<T> funcion)
+    {
+        IList<T> secuenciaResultante = new List<T>();
+
+        foreach (T elemento in secuencia)
+        {
+            if (funcion(elemento))
+                secuenciaResultante.Add(elemento);
+        }
+        return secuenciaResultante;
+    }
+
+    public static R? Reduce<T,R>(IEnumerable<T> secuencia, Func<T, R?, R?> funcion, R? acumulador = default(R))
+    {
+
+        foreach (T elemento in secuencia)
+        {
+            acumulador = funcion(elemento, acumulador);
+        }
+
+        return acumulador;
+    }
+
+
+    public static void Main()
+    {
+
+        //EjemplosImperativos();
+        EjercicioTransacciones();
+        EjercicioZip();
+    }
+
+    private static double Mitad(int v){
+        return v / 2.0;
+    }
+
+    public static void EjemplosImperativos()
+    {
+        int[] valores = { 1, 2, 3, 4, 5, 6 };
+        
+        // Ejemplo 1.
+
+        IList<double> resultante = new List<double>();
+        // Iteración del origen
+        foreach(var v in valores)
+        {
+            // Operación de transformación de cada elemento
+            double mitad = v / 2.0;
+
+            // Almacenamiento o emisión del elemento resultante
+            resultante.Add(mitad);
+        }
+
+        var resultMap = Map(valores, v => v / 2.0);
+        var resultMapMalo = Map(valores, Mitad);
+
+        Console.WriteLine("La mitad de cada elemento:");
+        foreach(var v in resultante)
+            Console.WriteLine(v);
+
+        // Ejemplo 2.
+
+        IList<double> resultante2 = new List<double>();
+        foreach(var v in valores)
+        {
+            if (v % 2 == 0)
+                resultante2.Add(v);
+        }
+
+        var resultFilter = Filter(valores, v => v % 2 == 0 );
+
+        Console.WriteLine("Los pares:");
+        foreach(var v in resultante2)
+            Console.WriteLine(v);
+
+
+        // Ejemplo 3.
+
+        int suma = 0;
+        foreach(var v in valores)
+            suma += v;
+
+        var resultReduce = Reduce<int,int>(valores, (v, acc) => v + acc);
+        Console.WriteLine($"La suma: {suma}");        
+    }
+
+
+    public static void EjercicioTransacciones()
+    {
+        var historicoVentas = new List<Venta>
+            {
+                new Venta { Region = "Europa",      Estado = Estado.Cancelada, Cantidad = 100m },
+                new Venta { Region = "Asia", Estado = Estado.Confirmada,  Cantidad = 500m },
+                new Venta { Region = "Europa",      Estado = Estado.Cancelada, Cantidad = 200m },
+                new Venta { Region = "Asia", Estado = Estado.Cancelada, Cantidad = 300m },
+                new Venta { Region = "Europa",      Estado = Estado.Cancelada, Cantidad = 50m },
+                new Venta { Region = "Asia", Estado = Estado.Cancelada, Cantidad = 150m },
+                new Venta { Region = "Europa",      Estado = Estado.Confirmada,  Cantidad = 120m },
+                new Venta { Region = "Asia", Estado = Estado.Cancelada, Cantidad = 800m },
+            };
+
+        // Cálculo del beneficio neto en Europa
+
+        // EJERCICIO: Parte a transformar 1.
+        decimal totalBeneficioEuropa = 0;
+        foreach (var v in historicoVentas)
+        {
+            if (v.Region.ToLower() == "europa")
+            {
+                if (v.Estado == Estado.Confirmada)
+                {
+                    decimal beneficioNeto = v.Cantidad * 0.80m;
+                    totalBeneficioEuropa += beneficioNeto;
+                }
+            }
+        }
+
+        var filterBeneficio = Filter(historicoVentas, v => v.Region.ToLower() == "europa" && v.Estado == Estado.Confirmada);
+        var resultReduceBeneficio = Reduce(filterBeneficio, (v, acc) => acc + v.Cantidad * 0.80m, 0m);
+
+        Console.WriteLine($"Beneficio neto en Europa: {resultReduceBeneficio} (Reduce)");
+        Console.WriteLine($"Beneficio neto en Europa: {totalBeneficioEuropa} (Imperativo)");
+
+
+        // Cálculo del beneficio medio.
+        decimal total = 0;
+        uint recuento = 0;
+        foreach (var s in historicoVentas)
+        {
+            if (s.Estado == Estado.Cancelada)
+            {
+                continue;
+            }
+            decimal margen = 0;
+            switch (s.Region.ToLower())
+            {
+                case "europa":
+                    margen = 0.80m;
+                    break;
+                case "asia":
+                    margen = 0.60m;
+                    break;
+                default:
+                    throw new Exception("Region desconocida");
+            }
+            total += s.Cantidad * margen;
+            recuento++;
+        }
+        decimal mediaBeneficio = total / recuento;
+
+        var filterMedia = Filter(historicoVentas, v => v.Estado == Estado.Confirmada);
+        var filterEuropa = Filter(filterMedia, v => v.Region.ToLower() == "europa");
+        var filterAsia = Filter(filterMedia, v => v.Region.ToLower() == "asia");
+
+        var resultReduceAsia = Reduce(filterAsia, (v, acc) => acc + v.Cantidad * 0.60m, 0m);
+        var resultReduceEuropa = Reduce(filterEuropa, (v, acc) => acc + v.Cantidad * 0.80m, 0m);
+
+        var resultReduceMedia = (resultReduceAsia + resultReduceEuropa) / (filterMedia.Count());
+
+        Console.WriteLine($"Beneficio medio: {resultReduceMedia} (Reduce)");   
+        Console.WriteLine($"Beneficio medio: {mediaBeneficio} (Imperativo)");        
+    }
+
+
+    /// <summary>
+    /// Implementa una función de orden superior Zip que reciba dos secuencias IEnumerable<T> y una función Func<T1, T2, TResult>. 
+    /// La función debe recorrer ambas secuencias en paralelo y devolver una nueva secuencia con el resultado de aplicar
+    /// la función a cada par de elementos (uno de cada secuencia -> Tupla). 
+    /// La iteración termina cuando se agote cualquiera de las dos secuencias.
+    /// </summary>
+    static void EjercicioZip()
+    {
+        //Imprímase por pantalla las tuplas resultantes de aplicar Zip a estas dos secuencias.
+        var regiones = new List<string> { "Europa", "África", "Asia" };
+        var margenes = new List<decimal> { 0.80m, 0.60m, 0.70m };
+
+        var zipResult = Zip(regiones, margenes, (r, m) => (Region: r, Margen: m));
+
+        Console.WriteLine("Tuplas resultantes de aplicar Zip:");
+        foreach (var tupla in zipResult)
+        {
+            Console.WriteLine($"Región: {tupla.Region}, Margen: {tupla.Margen}");
+        }
+
+    }
+
+    private static IEnumerable<TResult> Zip<T1, T2, TResult>(IEnumerable<T1> secuencia1, IEnumerable<T2> secuencia2, Func<T1, T2, TResult> funcion)
+    {
+        IList<TResult> secuenciaResultante = new List<TResult>();
+
+        using (var enumerador1 = secuencia1.GetEnumerator())
+        using (var enumerador2 = secuencia2.GetEnumerator())
+        {
+            while (enumerador1.MoveNext() && enumerador2.MoveNext())
+            {
+                TResult resultado = funcion(enumerador1.Current, enumerador2.Current);
+                secuenciaResultante.Add(resultado);
+            }
+        }
+
+        return secuenciaResultante;
+    }
+}
+public enum Estado
+{
+    Cancelada,
+    Confirmada
+}
+
+public class Venta
+{
+    public required string Region { get; set; }
+    public Estado Estado { get; set; }
+    public decimal Cantidad { get; set; }
+}
